982b7f14bc4424012238334a897b2a7cf966c8a9
HenriqueCarvalho97 <hnrique.carvalho@gmail.com>
2020-05-24 03:42:53 +0100

    new load balancer

---
+++ LB-AS/LoadBalancerAutoScaler.java
@@ -0,0 +1,160 @@
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.cloudwatch.AmazonCloudWatch;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.model.DescribeInstancesResult;
+import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.Reservation;
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class LoadBalancerAutoScaler {
+
+    static AmazonEC2      ec2;
+    static AmazonCloudWatch cloudWatch;
+
+    private static void init() throws Exception {
+        /*
+         * The ProfileCredentialsProvider will return your [default]
+         * credential profile by reading from the credentials file located at
+         * (~/.aws/credentials).
+         */
+
+        AWSCredentials credentials = null;
+        try {
+            credentials = new ProfileCredentialsProvider().getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException(
+                    "Cannot load the credentials from the credential profiles file. " +
+                            "Please make sure that your credentials file is at the correct " +
+                            "location (~/.aws/credentials), and is in valid format.",
+                    e);
+        }
+        ec2 = AmazonEC2ClientBuilder.standard().withRegion("us-east-1").withCredentials(new AWSStaticCredentialsProvider(credentials)).build();
+    }
+
+    public static void main(String[] args) throws Exception {
+        init();
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/sudoku", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+
+    public static String parseRequestBody(InputStream is) throws IOException {
+        InputStreamReader isr =  new InputStreamReader(is,"utf-8");
+        BufferedReader br = new BufferedReader(isr);
+
+        // From now on, the right way of moving from bytes to utf-8 characters:
+
+        int b;
+        StringBuilder buf = new StringBuilder(512);
+        while ((b = br.read()) != -1) {
+            buf.append((char) b);
+
+        }
+
+        br.close();
+        isr.close();
+
+        return buf.toString();
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+
+            try {
+                DescribeInstancesResult describeInstancesResult = ec2.describeInstances();
+                List<Reservation> reservations = describeInstancesResult.getReservations();
+                Set<Instance> instances = new HashSet<Instance>();
+
+                for (Reservation reservation : reservations) {
+                    instances.addAll(reservation.getInstances());
+                }
+                for (Instance instance : instances) {
+                    if(instance.getPublicDnsName().equals("ec2-54-157-232-222.compute-1.amazonaws.com"))
+                        continue;
+                    if (instance.getState().getName().equals("running"))
+                        forwardRequest(t, instance.getPublicDnsName());
+              }
+            } catch (AmazonServiceException ase) {
+                System.out.println("Caught Exception: " + ase.getMessage());
+                System.out.println("Reponse Status Code: " + ase.getStatusCode());
+                System.out.println("Error Code: " + ase.getErrorCode());
+                System.out.println("Request ID: " + ase.getRequestId());
+            }
+        }
+    }
+
+    private static void forwardRequest(HttpExchange t, String urlString) throws IOException {
+        String body = parseRequestBody(t.getRequestBody());
+
+        HttpURLConnection con = null;
+        try{
+            URL url = new URL("http://" + urlString + ":8000" + t.getRequestURI().toString());
+            con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Content-Type", "text/plain");
+            con.setRequestProperty("Content-Length", Integer.toString(body.getBytes("UTF-8").length));
+            con.setDoOutput(true);
+            OutputStream op = con.getOutputStream();
+            op.write(body.getBytes("UTF-8"));
+            op.close();
+
+            int status = con.getResponseCode();BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            in.close();
+
+            final Headers hdrs = t.getResponseHeaders();
+
+            hdrs.add("Content-Type", "application/json");
+
+            hdrs.add("Access-Control-Allow-Origin", "*");
+
+            hdrs.add("Access-Control-Allow-Credentials", "true");
+            hdrs.add("Access-Control-Allow-Methods", "POST, GET, HEAD, OPTIONS");
+            hdrs.add("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
+            System.out.println(content);
+
+            t.sendResponseHeaders(200, content.length());
+            OutputStream os = t.getResponseBody();
+            os.write(String.valueOf(content).getBytes());
+            os.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (con != null) {
+                con.disconnect();
+            }
+        }
+    }
+
+}
